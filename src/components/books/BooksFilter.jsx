7efc828169f3c011c91808dd330020a26d2059ea
Lazizbek <oddiybolakay05@gmail.com>
2024-12-19 01:39:05 +0500

    env file added

--- src/components/books/BooksFilter.jsx
+++ src/components/books/BooksFilter.jsx
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
 import { useDebounce } from "../../hooks/useDebounce/useDebounce";
 import { BookCard } from "./BookCard";
-import { Select, Input, Form, Button, Checkbox, Spin, Flex } from "antd";
+import {
+  Select,
+  Input,
+  Form,
+  Button,
+  Checkbox,
+  Spin,
+  Flex,
+  Switch,
+} from "antd";
 import { ClearOutlined, SearchOutlined } from "@ant-design/icons";
 import { useGetList } from "../../service/query/useGetList";
 import {
@@ -95,6 +104,12 @@ export const BooksFilter = () => {
     setSearchTriggered(false);
   };
 
+  const [visible, setVisible] = useState(false);
+
+  const handleSwitchChange = (checked) => {
+    setVisible(checked);
+  };
+
   if (error) {
     return <div className="text-red-500 text-center">Xatolik yuz berdi</div>;
   }
@@ -104,210 +119,221 @@ export const BooksFilter = () => {
       <h2 className="text-xl font-semibold mb-4 text-gray-800 text-center">
         Kitob qidirish uchun filtrlar
       </h2>
-      <Form layout="vertical">
-        <div className="grid grid-cols-1 md:grid-cols-5 gap-2 mb-4">
-          <Form.Item label="Sotuvchi">
-            <Select
-              placeholder="Sotuvchini tanlang"
-              value={filters.seller_id}
-              onChange={(value) => handleChange("seller_id", value)}
-              className="rounded-md border border-gray-300"
-            >
-              <Option value="">Hammasi</Option>
-              {users?.users?.map((users) => (
-                <Option key={users.id} value={users.id}>
-                  {users.role}: {users.email ? users.email : users.phone_number}
-                </Option>
-              ))}
 
-              {publishers?.publishers?.map((publisher) => (
-                <Option key={publisher.id} value={publisher.id}>
-                  publisher: {publisher.name}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
+      <Switch
+        checked={visible}
+        onChange={handleSwitchChange}
+        checkedChildren="ON"
+        unCheckedChildren="OFF"
+      />
 
-          <Form.Item label="Muallif">
-            <Select
-              placeholder="Muallifni tanlang"
-              value={filters.author_id}
-              onChange={(value) => handleChange("author_id", value)}
-              className="rounded-md border border-gray-300"
-            >
-              <Option value="">Hammasi</Option>
-              {authors?.authors?.map((author) => (
-                <Option key={author.id} value={author.id}>
-                  {author.name}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
+      {visible && (
+        <Form className="show-hide" layout="vertical">
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-2 mb-4">
+            <Form.Item label="Sotuvchi">
+              <Select
+                placeholder="Sotuvchini tanlang"
+                value={filters.seller_id}
+                onChange={(value) => handleChange("seller_id", value)}
+                className="rounded-md border border-gray-300"
+              >
+                <Option value="">Hammasi</Option>
+                {users?.users?.map((users) => (
+                  <Option key={users.id} value={users.id}>
+                    {users.role}:{" "}
+                    {users.email ? users.email : users.phone_number}
+                  </Option>
+                ))}
 
-          <Form.Item label="Nashriyot">
-            <Select
-              placeholder="Nashriyotni tanlang"
-              value={filters.publisher_id}
-              onChange={(value) => handleChange("publisher_id", value)}
-              className="rounded-md border border-gray-300"
-            >
-              <Option value="">Hammasi</Option>
-              {publishers?.publishers?.map((publisher) => (
-                <Option key={publisher.id} value={publisher.id}>
-                  {publisher.name}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
+                {publishers?.publishers?.map((publisher) => (
+                  <Option key={publisher.id} value={publisher.id}>
+                    publisher: {publisher.name}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
 
-          <Form.Item label="Kategoriya">
-            <Select
-              placeholder="Kategoriyani tanlang"
-              value={filters.category_id}
-              onChange={(value) => handleChange("category_id", value)}
-              className="rounded-md border border-gray-300"
-            >
-              <Option value="">Hammasi</Option>
-              {categories?.Categories?.categories.map((category) => (
-                <Option key={category.id} value={category.id}>
-                  {category?.name?.uz}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
+            <Form.Item label="Muallif">
+              <Select
+                placeholder="Muallifni tanlang"
+                value={filters.author_id}
+                onChange={(value) => handleChange("author_id", value)}
+                className="rounded-md border border-gray-300"
+              >
+                <Option value="">Hammasi</Option>
+                {authors?.authors?.map((author) => (
+                  <Option key={author.id} value={author.id}>
+                    {author.name}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
 
-          <Form.Item label="Tarjimon">
-            <Select
-              placeholder="Tarjimoni tanlang"
-              value={filters.translator_id}
-              onChange={(value) => handleChange("translator_id", value)}
-              className="rounded-md border border-gray-300"
-            >
-              <Option value="">Hammasi</Option>
-              {translators?.translators?.map((translator) => (
-                <Option key={translator.id} value={translator.id}>
-                  {translator.name}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
+            <Form.Item label="Nashriyot">
+              <Select
+                placeholder="Nashriyotni tanlang"
+                value={filters.publisher_id}
+                onChange={(value) => handleChange("publisher_id", value)}
+                className="rounded-md border border-gray-300"
+              >
+                <Option value="">Hammasi</Option>
+                {publishers?.publishers?.map((publisher) => (
+                  <Option key={publisher.id} value={publisher.id}>
+                    {publisher.name}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
 
-          <Form.Item label="Til">
-            <Select
-              placeholder="Tilni tanlang"
-              value={filters.language_id}
-              onChange={(value) => handleChange("language_id", value)}
-              className="rounded-md border border-gray-300"
-            >
-              <Option value="">Hammasi</Option>
-              {languages?.languages?.languages?.map((language) => (
-                <Option key={language.id} value={language.id}>
-                  {language?.name?.uz}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
+            <Form.Item label="Kategoriya">
+              <Select
+                placeholder="Kategoriyani tanlang"
+                value={filters.category_id}
+                onChange={(value) => handleChange("category_id", value)}
+                className="rounded-md border border-gray-300"
+              >
+                <Option value="">Hammasi</Option>
+                {categories?.Categories?.categories.map((category) => (
+                  <Option key={category.id} value={category.id}>
+                    {category?.name?.uz}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
 
-          <Form.Item label="Shahar">
-            <Select
-              placeholder="Shaharning tanlang"
-              value={filters.city_id}
-              onChange={(value) => handleChange("city_id", value)}
-              className="rounded-md border border-gray-300"
-            >
-              <Option value="">Hammasi</Option>
-              {cities?.Cities?.cities.map((city) => (
-                <Option key={city.id} value={city.id}>
-                  {city?.name?.uz}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
+            <Form.Item label="Tarjimon">
+              <Select
+                placeholder="Tarjimoni tanlang"
+                value={filters.translator_id}
+                onChange={(value) => handleChange("translator_id", value)}
+                className="rounded-md border border-gray-300"
+              >
+                <Option value="">Hammasi</Option>
+                {translators?.translators?.map((translator) => (
+                  <Option key={translator.id} value={translator.id}>
+                    {translator.name}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
 
-          <Form.Item label="Tuman">
-            <Select
-              placeholder="Tumanni tanlang"
-              value={filters.district_id}
-              onChange={(value) => handleChange("district_id", value)}
-              className="rounded-md border border-gray-300"
-            >
-              <Option value="">Hammasi</Option>
-              {districts?.Districts?.districts.map((district) => (
-                <Option key={district.id} value={district.id}>
-                  {district?.name?.uz}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
+            <Form.Item label="Til">
+              <Select
+                placeholder="Tilni tanlang"
+                value={filters.language_id}
+                onChange={(value) => handleChange("language_id", value)}
+                className="rounded-md border border-gray-300"
+              >
+                <Option value="">Hammasi</Option>
+                {languages?.languages?.languages?.map((language) => (
+                  <Option key={language.id} value={language.id}>
+                    {language?.name?.uz}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
 
-          <Form.Item label="Yozish turi">
-            <Select
-              placeholder="Yozish turini tanlang"
-              value={filters.writing_type}
-              onChange={(value) => handleChange("writing_type", value)}
-              className="rounded-md border border-gray-300"
-            >
-              <Option value="">Hammasi</Option>
-              <Option value="latin">Latin</Option>
-              <Option value="cyrillic">Cyrillic</Option>
-            </Select>
-          </Form.Item>
+            <Form.Item label="Shahar">
+              <Select
+                placeholder="Shaharning tanlang"
+                value={filters.city_id}
+                onChange={(value) => handleChange("city_id", value)}
+                className="rounded-md border border-gray-300"
+              >
+                <Option value="">Hammasi</Option>
+                {cities?.Cities?.cities.map((city) => (
+                  <Option key={city.id} value={city.id}>
+                    {city?.name?.uz}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
 
-          <Form.Item label="Status">
-            <Select
-              placeholder="Statusni tanlang"
-              value={filters.status}
-              onChange={(value) => handleChange("status", value)}
-              className="rounded-md border border-gray-300"
-            >
-              <Option value="">Hammasi</Option>
-              <Option value="active">Active</Option>
-              <Option value="non-active">Non-active</Option>
-            </Select>
-          </Form.Item>
+            <Form.Item label="Tuman">
+              <Select
+                placeholder="Tumanni tanlang"
+                value={filters.district_id}
+                onChange={(value) => handleChange("district_id", value)}
+                className="rounded-md border border-gray-300"
+              >
+                <Option value="">Hammasi</Option>
+                {districts?.Districts?.districts.map((district) => (
+                  <Option key={district.id} value={district.id}>
+                    {district?.name?.uz}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
 
-          <Flex vertical justify="center">
-            <Checkbox
-              className="border-2 p-[4px] rounded-md mt-1 bg-white"
-              checked={filters.is_new}
-              onChange={(e) => handleChange("is_new", e.target.checked)}
-            >
-              Yangi kitob
-            </Checkbox>
-          </Flex>
-        </div>
+            <Form.Item label="Yozish turi">
+              <Select
+                placeholder="Yozish turini tanlang"
+                value={filters.writing_type}
+                onChange={(value) => handleChange("writing_type", value)}
+                className="rounded-md border border-gray-300"
+              >
+                <Option value="">Hammasi</Option>
+                <Option value="latin">Latin</Option>
+                <Option value="cyrillic">Cyrillic</Option>
+              </Select>
+            </Form.Item>
 
-        <Flex wrap gap={24}>
-          <Flex className="max-w-lg w-full">
-            <Input
-              suffix={<SearchOutlined />}
-              size="large"
-              placeholder="Kitob nomi bo'yicha qidiring"
-              value={filters.title}
-              onChange={(e) => handleChange("title", e.target.value)}
-            />
-          </Flex>
-          <Button
-            size="large"
-            type="primary"
-            onClick={handleSearch}
-            block
-            className="max-w-96"
-          >
-            Qidirish
-          </Button>
-          {searchTriggered && (
+            <Form.Item label="Status">
+              <Select
+                placeholder="Statusni tanlang"
+                value={filters.status}
+                onChange={(value) => handleChange("status", value)}
+                className="rounded-md border border-gray-300"
+              >
+                <Option value="">Hammasi</Option>
+                <Option value="active">Active</Option>
+                <Option value="non-active">Non-active</Option>
+              </Select>
+            </Form.Item>
+
+            <Flex vertical justify="center">
+              <Checkbox
+                className="border-2 p-[4px] rounded-md mt-1 bg-white"
+                checked={filters.is_new}
+                onChange={(e) => handleChange("is_new", e.target.checked)}
+              >
+                Yangi kitob
+              </Checkbox>
+            </Flex>
+          </div>
+
+          <Flex wrap gap={24}>
+            <Flex className="max-w-lg w-full">
+              <Input
+                suffix={<SearchOutlined />}
+                size="large"
+                placeholder="Kitob nomi bo'yicha qidiring"
+                value={filters.title}
+                onChange={(e) => handleChange("title", e.target.value)}
+              />
+            </Flex>
             <Button
               size="large"
-              icon={<ClearOutlined />}
-              onClick={handleClear}
-              className="max-w-md bg-red-500 text-white"
+              type="primary"
+              onClick={handleSearch}
+              block
+              className="max-w-96"
             >
-              Tozalash
+              Qidirish
             </Button>
-          )}
-        </Flex>
-      </Form>
+            {searchTriggered && (
+              <Button
+                size="large"
+                icon={<ClearOutlined />}
+                onClick={handleClear}
+                className="max-w-md bg-red-500 text-white"
+              >
+                Tozalash
+              </Button>
+            )}
+          </Flex>
+        </Form>
+      )}
 
       <div>
         {isLoading ? (
